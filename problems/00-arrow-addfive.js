71697b8b78b19a74ea37e28de8319b565633af78
SunTheCoder <sunenglishjr@gmail.com>
2024-04-03 20:37:31 -0400

    second commit

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,7 +15,7 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
