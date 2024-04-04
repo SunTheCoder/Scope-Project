aaa0b7b713d9494c47fa6b1d6aa196a5aa029143
SunTheCoder <sunenglishjr@gmail.com>
2024-04-03 21:06:39 -0400

    01-solved/driver-Sun/nav-April

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -7,15 +7,16 @@ keyword will allow any value assigned to that variable protection from being
 reassigned within that scope.
 
 Examples:
-let p1 = {firstName: 'John', lastName: 'Doe'};
-arrowGetFullName(p1); // => 'John Doe'
-let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
-arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
 
-// Your code here 
+const arrowGetFullName = (person) => person.firstName + ' ' + person.lastName
 
+
+let p1 = {firstName: 'John', lastName: 'Doe'};
+console.log(arrowGetFullName(p1)); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+console.log(arrowGetFullName(p2)); // => 'Charlie Brown'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
