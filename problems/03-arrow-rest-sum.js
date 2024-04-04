c49e32bc49fef407369c2ea8b33d829a0f7b3d11
SunTheCoder <sunenglishjr@gmail.com>
2024-04-04 19:10:43 -0400

    03-solved/driver-Sun/nav-April

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -6,13 +6,29 @@ arrow function syntax.
 **Hint**: Use rest parameter syntax!
 
 Examples:
-arrowRestSum(3,5,6); // => 14
-arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
-arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+const arrowRestSum = (...nums) => {
+  
+  let sum = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    
+    sum += num
+    
+    
+  }
+  
+  return sum
+  
+}
 
+//const arrowRestSum = (...nums) => nums.reduce((acc, num) => acc + num, 0)
+
+console.log(arrowRestSum(3,5,6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
