5f3ea4dddd7a801e6193f928cd0043db0f867795
aprilsmartt <20917096+aprilsmartt@users.noreply.github.com>
2024-04-04 19:56:40 -0500

    09-solved/driver-april/nav-sun

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -16,7 +16,25 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here 
+const interrupter = (interruptingWord) => {
+  return function(sentence) {
+    // let words = sentence.split(" "); 
+    // return words.join(" " + interruptingWord + " ")
+    return sentence.split(" ").join(" " + interruptingWord + " ")
+
+  }
+}
+
+// Look below to see how this function is invoked:
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
+
+// Invoking the interrupter function again:
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
