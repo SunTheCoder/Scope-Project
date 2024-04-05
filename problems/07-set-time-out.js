a41f49995401dfe3a68735516e7a6a5a02b77290
aprilsmartt <20917096+aprilsmartt@users.noreply.github.com>
2024-04-04 19:15:33 -0500

    07-solved/driver-april/nav-sun

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -9,16 +9,19 @@ test your answer!
 
 Examples:
 
+
+
+***********************************************************************/
+
+const funcTimer = (time, func) => {
+  return setTimeout(func, time)
+}
+
 function partyFunc () {
   console.log("Party time!")
 }
 
 funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
-
-
-***********************************************************************/
-
-// Your code here 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
