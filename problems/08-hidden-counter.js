6d6c9c061f860b7bd1a10745a9e19ea02c87d9aa
SunTheCoder <sunenglishjr@gmail.com>
2024-04-04 20:23:35 -0400

    08-solved/driver-sun/nav-april

--- problems/08-hidden-counter.js
+++ problems/08-hidden-counter.js
@@ -15,7 +15,16 @@ hidden2(); // returns 1
 
 ***********************************************************************/
 
-// Your code here 
+const hiddenCounter = () => {
+
+    let count = 0;
+
+    return function() {
+      count++
+
+      return count
+    }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
